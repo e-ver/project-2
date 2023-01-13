bd86a9cbff2ae782a4ceaef9c6f074eb77ffb02a
Eduard Vervikishko <eddoo@yandex.ru>
2023-01-13 03:18:00 +0000

    lastchange

--- practic2.py
+++ practic2.py
@@ -1,12 +1,13 @@
 def spce():
-  print('\n__________________________________________')
+    print('\n__________________________________________')
 def counts(x):
-  count = 0
-  while x > 0:
-    x //= 10
-    count += 1
-  return count
+    count = 0
+    while x > 0:
+        x //= 10
+        count += 1
+    return count
 spce()
 x = 233
 print(counts(x))
-print('XOXOXO')
+print('XOXOXO', x)
+print('__________________________________________')
